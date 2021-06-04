9fcfa18ff22e260abf1b436e568ac01a54061c46
Dung278 <81941929+Dung278@users.noreply.github.com>
2021-06-04 08:55:46 +0700

    Update TestLab01_1216586.java

--- TestLab01_1216586.java
+++ TestLab01_1216586.java
@@ -3,7 +3,18 @@ package lab01_1216586;
 import java.util.Scanner;
 
 public class TestLab01_1216586
-{
+{	
+	//check Prime number
+	public static boolean Prime(int n){
+		if(n<2)
+			return false;
+		for(int i=0;i<n;i++){
+			if(n%i==0){
+				return false;
+			}
+		}
+		return true;
+	}
 	public static void main(String[] args)
 	{
 		int i,j,k=1,n;  
